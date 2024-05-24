2aa661c76af209d83ae03accc882e9ff8139bc81
tosiab <mtabednarz@gmail.com>
2024-05-24 11:03:42 -0400

    robot

--- ApplicationRobot.java
+++ ApplicationRobot.java
@@ -1,5 +1,12 @@
 import java.awt.*;
 import java.awt.event.InputEvent;
+import javax.imageio.ImageIO;
+import java.awt.event.*;
+import java.awt.Image;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
 
 public class ApplicationRobot{
     public static void main(String[]args) throws Exception{
@@ -8,20 +15,36 @@ public class ApplicationRobot{
         tosia.mousePress(InputEvent.BUTTON1_DOWN_MASK);
         tosia.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
         tosia.delay(500);
-        tosia.mouseMove(100,883);
+        tosia.mouseMove(100,725);
         tosia.mousePress(InputEvent.BUTTON1_DOWN_MASK);
         tosia.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
         tosia.delay(500);
         tosia.mouseMove(1150,525);
         tosia.delay(800);
         tosia.mousePress(InputEvent.BUTTON1_DOWN_MASK);
-        tosia.delay(500);
+        tosia.delay(800);
         tosia.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
         tosia.delay(500);
         tosia.mouseMove(1150,925);
-        tosia.delay(500);
+        tosia.delay(800);
         tosia.mousePress(InputEvent.BUTTON1_DOWN_MASK);
         tosia.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+        tosia.delay(500);
+        tosia.mouseMove(700,100);
+        tosia.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+        tosia.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+        tosia.delay(500);
+        tosia.mouseMove(700,200);
+        tosia.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+        tosia.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        Rectangle bounds = new Rectangle(screenSize);
+        BufferedImage image= tosia.createScreenCapture(bounds); 
+        File file = new File("myScreenShot.png");
+        ImageIO.write(image,"png", file);
+        System.out.println("A screenshot is captured to " + file.getPath()); 
+
+
 
         
       
